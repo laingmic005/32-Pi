de2c886a225b743dabf9ceb5ffe05742e793a836
Isabell Hudnall <91754435+laingmic005@users.noreply.github.com>
2026-06-06 18:40:14 -0600

    Update fpga_pins_on.py

--- fpga-pins-on/fpga_pins_on.py
+++ fpga-pins-on/fpga_pins_on.py
@@ -1,5 +1,5 @@
 '''
-Code written by Micah Laing on 2024-10-23 for the UVU Mechatronics 32 Pi project.
+Code written by Isabell Hdunall on 2024-10-23 for the UVU Mechatronics 32 Pi project.
 This program will generate a Verilog file to turn on all the pins on the FPGA.
 '''
 from myhdl import block, instance, Signal, delay
